9e1f89675b14bcdbe51f36bac1626a00c3ff3c05
Precious Agboinghale <97043576+Oat-mea@users.noreply.github.com>
2022-07-18 20:29:52 +0000

    Yarn test

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,10 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+const sum = arrayOfNumbers.reduce ((a,b) => a+b);
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +29,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0;
+  for(let i =1; i < arrayOfNumbers.length; i++){
+    if(arrayOfNumbers[i] % 2 === 0) {
+      count++
+    }
+  }
+  return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
